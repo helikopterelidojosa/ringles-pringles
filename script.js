12dcfcc34f6f1c01422427343722f9fb795b1f8d
codesoup <82330389+helikopterelidojosa@users.noreply.github.com>
2023-01-26 21:33:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,21 +6,33 @@ let ch = canvas.height = innerHeight;
 
 
 
-var radius = Math.random()*10;
-var x = cw;
-var x2 = 0;
-var y = ch / 2;
-var y2 = ch / 2;
-var step = Math.random()*6;
-var setLineWidth = Math.random()*2;
-
-var hue = Math.random()*360;
-var light = Math.random()*100;
-var saturation = Math.random()*100;
 
 
 
-var bhue = Math.random()*360;
+
+
+
+
+
+
+
+
+
+
+function drawMovingCircles(){
+    var radius = Math.random()*10;
+    var x = cw;
+    var x2 = 0;
+    var y = ch / 2;
+    var y2 = ch / 2;
+    var step = Math.random()*6;
+    var setLineWidth = Math.random()*2;
+    
+    var hue = Math.random()*360;
+    var light = Math.random()*100;
+    var saturation = Math.random()*100;
+
+    var bhue = Math.random()*360;
 var blight = Math.random()*50;
 var bsaturation = Math.random()*100;
 
@@ -29,14 +41,6 @@ ctx.fillStyle = 'hsl('+(370*bhue)+',' + bsaturation + '%,'+(blight)+'%)';
 ctx.fillRect(0,0,1920,1080);
 }
 
-
-//set url for saving
-function setURL(){
-    link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
-}
-
-
-function drawMovingCircles(){
 drawBackground();
 
     for(i=0; i<500; i++){
@@ -69,32 +73,38 @@ ctx.lineWidth = setLineWidth;
 ctx.stroke();
 
     }
-    
-   
-    
-
-
-
-
    
 
 
 
 
+
+   
+
+
+
+
+  
 }
 
-function save(){
-    
-    
-    var link = document.getElementById('link');
-    link.setAttribute('download', `zupart.png`);
-    setURL();
-    
-}
-save()
 
-drawMovingCircles();
+    
 
-function save2(){
+
+
+
+
+
+    
+    drawMovingCircles();
+   
+
+
+
+
+function saveCanvas() {
+    var link = document.createElement('a');
+    link.download = 'myCanvas.png';
+    link.href = canvas.toDataURL();
     link.click();
 }
